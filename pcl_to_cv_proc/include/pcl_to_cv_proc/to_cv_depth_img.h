755f12343c295fb09929dfb2fd1a781b4cab7323
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2016-03-13 13:26:20 +0100

    Package skeleton

---
+++ pcl_to_cv_proc/include/pcl_to_cv_proc/to_cv_depth_img.h
@@ -0,0 +1,66 @@
+//=================================================================================================
+// Copyright (c) 2016, Stefan Kohlbrecher, TU Darmstadt
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Simulation, Systems Optimization and Robotics
+//       group, TU Darmstadt nor the names of its contributors may be used to
+//       endorse or promote products derived from this software without
+//       specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//=================================================================================================
+
+#ifndef TO_CV_DEPTH_IMG_H___
+#define TO_CV_DEPTH_IMG_H___
+
+#include <opencv/cv.h>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <pcl/point_cloud.h>
+#include <pcl/range_image/range_image_planar.h>
+
+#include <sensor_msgs/PointCloud2.h>
+#include <geometry_msgs/Pose.h>
+
+namespace pcl_to_cv_proc{
+
+bool generateDepthImage(const sensor_msgs::PointCloud2& cloud,
+                        const Eigen::Affine3d& camera_pose,
+                        pcl::RangeImagePlanar& range_image,
+                        cv::Mat& out,
+                        geometry_msgs::Pose& camera_pose)
+{
+
+  return true;
+}
+
+template <typename PointT>
+bool generateDepthImage(const boost::shared_ptr<pcl::PointCloud<PointT> >& cloud,
+                        const Eigen::Affine3d& camera_pose,
+                        pcl::RangeImagePlanar& range_image,
+                        cv::Mat& out)
+{
+
+  //range_img.createFromPointCloudWithFixedSize
+  return true;
+}
+
+}
+
+#endif
