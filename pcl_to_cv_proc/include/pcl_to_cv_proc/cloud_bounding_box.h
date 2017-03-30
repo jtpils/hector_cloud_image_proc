4cf740d7712edb45fc3804e305439abbb511b51d
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2017-03-30 14:17:04 +0200

    Fix kinetic compile issue

--- pcl_to_cv_proc/include/pcl_to_cv_proc/cloud_bounding_box.h
+++ pcl_to_cv_proc/include/pcl_to_cv_proc/cloud_bounding_box.h
@@ -31,7 +31,16 @@
 #ifndef CLOUD_BOUNDING_BOX_H___
 #define CLOUD_BOUNDING_BOX_H___
 
-#include <opencv/cv.h>
+
+// Upper part verified with cv 2.4, lower with 3.x
+// Needs testing if the 3.x include also works on indigo
+#if CV_MAJOR_VERSION == 2
+  #include <opencv/cv.h>
+#elif CV_MAJOR_VERSION == 3
+  #include <opencv2/imgproc/imgproc.hpp>
+#endif
+
+
 
 #include <pcl/point_cloud.h>
 
